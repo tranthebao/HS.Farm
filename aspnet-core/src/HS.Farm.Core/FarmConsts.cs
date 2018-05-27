6f51f0b00fff2c17db0a4a4315fe37b6ddd19cb3
UchihaMadara-PC\UchihaMadara <tranthebaocntt31b@gmail.com>
2018-05-27 21:44:04 +0700

    [modify] hs-farm init-first

---
+++ aspnet-core/src/HS.Farm.Core/FarmConsts.cs
@@ -0,0 +1,11 @@
+﻿namespace HS.Farm
+{
+    public class FarmConsts
+    {
+        public const string LocalizationSourceName = "Farm";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+    }
+}
